cf35e6d6f9b56c7ef839ba569d095bf032d4d885
Gabriel Rulo <gabriel.srulo@gmail.com>
2021-04-29 09:38:48 -0300

    Entrega da Atividade

--- barreamento-de-eventos/src/index.js
+++ barreamento-de-eventos/src/index.js
@@ -1,24 +1,60 @@
 const express = require("express");
+//para enviar eventos para os demais microsserviços
 const axios = require("axios");
+
 const app = express();
 app.use(express.json());
 
 const eventos = [];
 
-app.post("/eventos", async(req, res) => {
-    const evento = req.body;
-    eventos.push(evento);
-    axios.post("http://localhost:8000/eventos",evento);
-    axios.post("http;//localhost:5000/eventos",evento);
-    axios.post("http://localhost:6000/eventos",evento);
-    axios.post("http://localhost:7000/eventos",evento);
-    res.status(200).send({
-        msg:"ok"
+app.post("/eventos", (req, res) => {
+  const evento = req.body;
+  eventos.push(evento);
+  //envia o evento para o microsserviço de Clientes
+  axios
+    .post("http://localhost:3333/eventos", evento)
+    .then((resp) => {
+      console.log(resp.data);
+    })
+    .catch((error) => {
+      console.log(error);
     });
+  //envia o evento para o microsserviço de Ingressos
+  axios
+    .post("http://localhost:4444/eventos", evento)
+    .then((resp) => {
+      console.log(resp.data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  //envia o evento para o microsserviço de Classificacao
+  axios
+    .post("http://localhost:5555/eventos", evento)
+    .then((resp) => {
+      console.log(resp.data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  //envia o evento para o microsserviço de Consulta
+  axios
+    .post("http://localhost:6666/eventos", evento)
+    .then((resp) => {
+      console.log(resp.data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  res.status(200).send({
+    msg: "ok",
+  });
 });
-app.get("/eventos",(req,res) => {
-    res.send(eventos);
+
+app.get("/eventos", (req, res) => {
+  res.send(eventos);
 });
-app.listen(10000, () => {
-    console.log("Barreamento de eventos. Porta 10000")
-})
+
+app.listen(7777, () => {
+  console.log("Barramento de eventos. Porta 7777.");
+});
