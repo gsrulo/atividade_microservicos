4098c4f9149c5ac10b9c0c27cec27e936a9f6eee
Gabriel Rulo <gabriel.srulo@gmail.com>
2021-04-28 21:28:00 -0300

    Commit

--- cadastro-clientes/src/index.js
+++ cadastro-clientes/src/index.js
@@ -6,33 +6,47 @@ const clientes = {};
 contador = 0;
 
 app.get("/clientes", (req, res) => {
-    res.send(clientes);
+  res.send(clientes);
 });
-app.put("/clientes", async(req, res) => { 
-    contador++;
-    const {
-        cliente: [
-            {
-                nome,
-                endereco,
-                idade: int,
-                status 
-            }
-        ]
-    } = req.body;
-    clientes[contador] = {
-        contador,
-        cliente
-    }
-    await axios.post("http://localhost:10000/eventos", {
-        tipo: "ClienteCriado",
-        dados: {
-            contador,
-            cliente,  
+app.put("/clientes", async (req, res) => {
+  contador++;
+  const {
+    cliente: [{ nome, endereco, idade, status }],
+  } = req.body;
+  clientes[contador] = {
+    contador,
+    cliente: [
+      {
+        nome,
+        endereco,
+        idade,
+        status,
+      },
+    ],
+  };
+  await axios.post("http://localhost:10000/eventos", {
+    tipo: "ClienteCriado",
+    dados: {
+      contador,
+      cliente: [
+        {
+          nome,
+          endereco,
+          idade,
+          status,
         },
-    });
-    res.status(201).send(clientes[contador]);
+      ],
+    },
+  });
+  res.status(201).send(clientes[contador]);
 });
-app.listen(8000, () => {
-    console.log("clientes. Porta 8000");
+app.delete("/clientes/:id", (req, res, next) => {
+  const idClienteDeletado = req.params.id;
+  clientes.forEach((cliente, index) => {
+    if (cliente.id == idClienteDeletado) clientes.splice(index);
+  });
+  res.status(201).json(clientes);
+});
+app.listen(4000, () => {
+  console.log("clientes. Porta 4000");
 });
